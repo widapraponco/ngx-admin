65fa40e460fdf6b9fe3679d9f05427ed5e3b6581
ariaresa <ariaresa93@gmail.com>
2022-07-15 14:37:06 +0700

    footer

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by">
+    <!-- <span class="created-by">
       Created with ♥ by <b><a href="https://akveo.page.link/8V2f" target="_blank">Akveo</a></b> 2019
     </span>
     <div class="socials">
@@ -12,7 +12,7 @@ import { Component } from '@angular/core';
       <a href="#" target="_blank" class="ion ion-social-facebook"></a>
       <a href="#" target="_blank" class="ion ion-social-twitter"></a>
       <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
-    </div>
+    </div> -->
   `,
 })
 export class FooterComponent {
