3b4e95f6beb8668c8a61a146c90374a4c7c7e4a0
wida praponco <widapraponco02@gmail.com>
2022-12-12 22:45:48 +0700

    add store folder

---
+++ src/app/@store/auth/user.model.ts
@@ -0,0 +1,12 @@
+
+export namespace Users {
+
+    export interface User {
+        created_at: string
+        email: string
+        email_verified_at: string
+        id: string
+        name: string
+        updated_at: string
+    }
+}
